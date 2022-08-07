cf14191c73220a2c70fb2f4da2e74bfbf270deda
Thorrell Turner <thorrellt@gmail.com>
2022-08-06 21:23:21 -0400

    init commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <p> hello world</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
